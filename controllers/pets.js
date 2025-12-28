b2f493e47c4355eccbff6fe977c6a7cb6ad4ce81
Nawaf-Aljar <nawaf.aljar@outlook.com>
2025-12-28 16:36:34 +0300

    Added the details

--- controllers/pets.js
+++ controllers/pets.js
@@ -14,5 +14,30 @@ router.post("/", async (req,res) => {
     }
 
 })
-
+router.get("/", async (req,res) => {
+    try{
+        const pets = await Pet.find({})
+        res.status(200).json({pets})
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({err: "Failed to get pets"})
+    }
+})
+router.get("/:id", async (req,res) => {
+    try{
+        const {id} = req.params
+        const pet = await Pet.findById(id)
+        if (!pet){
+            res.status(404).json({err:"Pet not found"})
+        }
+        else{
+            res.status(200).json({pet})
+        }
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({err: "Failed to get pet"})
+    }
+})
 module.exports = router
